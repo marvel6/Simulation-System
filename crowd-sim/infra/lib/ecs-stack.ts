f2d3421defdcaaf9657cc102bac3097d2c20f694
Marvellous Solomon <marvelloussolomon49@gmail.com>
2026-08-13 00:23:36 +0100

    Refactor EcsStack to use redisUrl and clientSecurityGroup

--- crowd-sim/infra/lib/ecs-stack.ts
+++ crowd-sim/infra/lib/ecs-stack.ts
@@ -5,30 +5,36 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export interface EcsStackProps extends cdk.StackProps {
     vpc: ec2.Vpc;
-    redisEndpoint: string;
+    redisUrl: string;
+    clientSecurityGroup: ec2.SecurityGroup;
 }
 
 export class EcsStack extends cdk.Stack {
+
+    private readonly redisUrl: string;
 
     constructor(scope: Construct, id: string, props: EcsStackProps) {
         super(scope, id, props);
 
         const cluster = new ecs.Cluster(this, `Ec2Cluster`, { vpc: props.vpc })
 
+        this.redisUrl = `redis://${props.redisUrl}`
+
         this.getPartitions().forEach(partition => {
+
+            const partitionId = `partition-${partition}`
 
             const taskDefinition = new ecs.FargateTaskDefinition(this, `${partition}-TaskDef`)
 
             taskDefinition.addContainer("CrowdSimContainer", {
-                image: ecs.ContainerImage.fromRegistry("crowd-sim:latest"),
+                image: ecs.ContainerImage.fromAsset('../services/simulation/Dockerfile'),
                 memoryLimitMiB: 512,
                 cpu: 256,
-                portMappings: [{
-                    containerPort: 8080,
-                }],
                 environment: {
-                    REDIS_ENDPOINT: props.redisEndpoint,
-                }
+                    REDIS_URL: this.redisUrl,
+                    PARTITION_ID: partitionId,
+                },
+                logging: new ecs.AwsLogDriver({ streamPrefix: partitionId }),
             })
 
             const service = new ecs.FargateService(this, `${partition}-CrowdSimService`, {
@@ -38,20 +44,17 @@ export class EcsStack extends cdk.Stack {
                 circuitBreaker: {
                     rollback: true,
                     enable: true
-                }
+                },
+                securityGroups: [props.clientSecurityGroup],
+                vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }
             })
 
             new cdk.CfnOutput(this, `${partition}-CrowdSimServiceOutput`, {
                 value: service.serviceName,
                 description: "The name of the CrowdSim service",
             })
-
-            cluster.addCapacity("DefaultCapicity", {
-                desiredCapacity: 1,
-                maxCapacity: 1,
-                instanceType: new ec2.InstanceType('t3.micro'),
-            })
         })
+
     }
 
     private getPartitions(): string[] {
