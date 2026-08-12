6bd743f6841d9519b7b97a820fa42be7bdd30b60
Marvellous Solomon <marvelloussolomon49@gmail.com>
2026-08-12 21:09:33 +0100

    Add RedisStack implementation for AWS CDK
    - Introduced RedisStack class to manage Redis infrastructure within the CDK project.
    - Configured security groups for Redis and client access.
    - Set up a subnet group and cache cluster for Redis deployment.
    - Added output for Redis endpoint for easy access.

---
+++ crowd-sim/infra/lib/redis-stack.ts
@@ -0,0 +1,55 @@
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib/core';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as elasticache from 'aws-cdk-lib/aws-elasticache';
+
+export interface RedisStackProps extends cdk.StackProps {
+    vpc: ec2.Vpc;
+}
+
+export class RedisStack extends cdk.Stack {
+    public readonly redisEndpoint: string;
+    public readonly securityGroup: ec2.SecurityGroup;
+    public readonly clientSecurityGroup: ec2.SecurityGroup;
+
+    constructor(scope: Construct, id: string, props: RedisStackProps) {
+        super(scope, id, props);
+
+        this.securityGroup = new ec2.SecurityGroup(this, 'CrowdSimRedisSecurityGroup', {
+            vpc: props.vpc,
+            description: 'Security group for CrowdSim Redis',
+            allowAllOutbound: false,
+        });
+
+        this.clientSecurityGroup = new ec2.SecurityGroup(this, 'CrowdSimRedisClientSecurityGroup', {
+            vpc: props.vpc,
+            description: 'Security group for CrowdSim Redis clients',
+            allowAllOutbound: true,
+        });
+
+        this.securityGroup.addIngressRule(this.clientSecurityGroup, ec2.Port.tcp(6379));
+
+
+        const subnetGroup = new elasticache.CfnSubnetGroup(this, 'ClusterSubnetGroup', {
+            description: 'Subnet group for CrowdSim Redis',
+            subnetIds: props.vpc.privateSubnets.map(subnets => subnets.subnetId)
+        })
+
+        const cluster = new elasticache.CfnCacheCluster(this, 'CrowdSimCluster', {
+            clusterName: 'CrowdSimCluster',
+            engine: 'redis',
+            cacheNodeType: 'cache.t3.micro',
+            numCacheNodes: 1,
+            vpcSecurityGroupIds: [this.securityGroup.securityGroupId],
+            cacheSubnetGroupName: subnetGroup.ref,
+        })
+
+        this.redisEndpoint = `${cluster.attrRedisEndpointAddress}:${cluster.attrRedisEndpointPort}`
+
+
+        new cdk.CfnOutput(this, "RedisEndpointOutput", {
+            value: this.redisEndpoint,
+            description: 'Redis endpoint',
+        });
+    }
+}
